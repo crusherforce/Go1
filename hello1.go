b047e23eb015cb35bf8e780532e56bc9d38841fd
Meghaditya Roy Chaudhury <meroycha@microsoft.com>
2020-08-05 01:55:11 +0530

    Changes done after antakshari

--- hello1.go
+++ hello1.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"net/http"
+	"hash/crc32"
+	"encoding/hex"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	fmt.Printf("Hello %s\n", new(World))
+
+	// Printing Durations
+	start := time.Now()
+
+	// Make an HTTP request
+	resp, _ := http.Get("http://www.google.com")
+	resp.Body.Close()
+	fmt.Println(time.Since(start))
+
+	h := crc32.NewIEEE()
+	fmt.Fprintf(h, "Hello World")
+	fmt.Printf("hash=%x\n", h.Sum32())
+
+	h1 := hex.Dumper(os.Stdout)
+	defer h1.Close()
+	fmt.Fprintf(h1, "Hello World\n")
 }
+
+type World struct{}
+
+func (w *World) String() string {
+	return "Crusherforce"
+}
+
+/*
+Notes
+------
+Ref: A Tour of Go (https://research.swtch.com/gotour)
+
+- A type implements an interface by defining the required methods
+*/
